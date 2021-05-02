2d6dd2898f78d0b656a784d3a03a0cb0e8fc1186
gowshiselva <gowshigan.selvarasa@aalto.fi>
2021-05-02 20:51:58 +0300

    add write function
    This implement functions to save results to putput file

--- write.py
+++ write.py
@@ -12,6 +12,8 @@ You'll edit this file in Part 4.
 """
 import csv
 import json
+from helpers import datetime_to_str
+
 
 
 def write_to_csv(results, filename):
@@ -26,7 +28,19 @@ def write_to_csv(results, filename):
     """
     fieldnames = ('datetime_utc', 'distance_au', 'velocity_km_s', 'designation', 'name', 'diameter_km', 'potentially_hazardous')
     #
+    with open(filename, 'w') as csv_to_write:
+        csv_writer = csv.DictWritter(csv_to_write, fieldnames = fieldnames)
+        csv_writer.writeheader()
 
+        for each_approach in results:
+            csv_writer.writerow({
+                'datetime_utc': datetime_to_str(each_approach.time),
+                'distance_au': each_approach.distance,
+                'velocity_km_s': each_approach.velocity,
+                'designation': each_approach.designation,
+                'name': each_approach.neo.name,
+                'diameter_km': each_approach.neo.diameter,
+                'potentially_hazardous': str(each_approach.neo.hazardous)})
 
 def write_to_json(results, filename):
     """Write an iterable of `CloseApproach` objects to a JSON file.
@@ -39,4 +53,22 @@ def write_to_json(results, filename):
     :param results: An iterable of `CloseApproach` objects.
     :param filename: A Path-like object pointing to where the data should be saved.
     """
-    # TODO: Write the results to a JSON file, following the specification in the instructions.
+    # Write the results to a JSON file, following the specification in the instructions.
+    dict_for_json = list()
+
+    for each_approach in results:
+        row_for_json = {
+            'datetime_utc': datetime_to_str(each_approach.time),
+            'distance_au': each_approach.distance,
+            'velocity_km_s': each_approach.velocity,
+            'neo': {
+                'designation': each_approach.designation,
+                'name': each_approach.neo.name,
+                'diameter_km': each_approach.neo.diameter,
+                'potentially_hazardous': each_approach.neo.hazardous
+            }
+        }
+        dict_for_json.append(row_for_json)
+
+    with open(filename, 'w') as json_to_write:
+        json_to_write.write(json.dumps(dict_for_json, indent=4))
